fa511fe51902a212314fe4840b21f4210f7cfade
GitHub Kullanıcı Adınız <GitHub E-Posta Adresini>
2023-11-05 00:03:14 +0400

    My App

---
+++ WPFStoreApp/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFStoreApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+
+
+        }
+
+
+
+
+        private void Cola_Click(object sender, RoutedEventArgs e)
+        {
+            Cola.Foreground = Brushes.Red;
+        }
+
+        private void Fanta_Click(object sender, RoutedEventArgs e)
+        {
+            Fanta.Foreground = Brushes.DarkOrange;
+        }
+
+        private void Sprite_Click(object sender, RoutedEventArgs e)
+        {
+            Sprite.Foreground = Brushes.Green;
+        }
+
+        private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == "Search...")
+            {
+                textBox.Text = string.Empty;
+            }
+        }
+
+        private void SearchBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Search...";
+            }
+        }
+
+        private void SearchBox_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void All_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cola1L_Click(object sender, RoutedEventArgs e)
+        {
+            Cola1L.Foreground = Brushes.Red;
+            Cola05L.Foreground = Brushes.Gray;
+            Fanta1L.Foreground = Brushes.Gray;
+            Fanta05L.Foreground = Brushes.Gray;
+            Sprite1L.Foreground = Brushes.Gray;
+            Sprite05L.Foreground = Brushes.Gray;
+        }
+
+        private void Cola05L_Click(object sender, RoutedEventArgs e)
+        {
+            Cola05L.Foreground = Brushes.Red;
+            Cola1L.Foreground = Brushes.Gray;
+            Fanta1L.Foreground = Brushes.Gray;
+            Fanta05L.Foreground = Brushes.Gray;
+            Sprite1L.Foreground = Brushes.Gray;
+            Sprite05L.Foreground = Brushes.Gray;
+        }
+
+        private void Fanta1L_Click(object sender, RoutedEventArgs e)
+        {
+            Fanta1L.Foreground = Brushes.DarkOrange;
+            Fanta05L.Foreground = Brushes.Gray;
+            Cola1L.Foreground= Brushes.Gray;
+            Cola05L.Foreground = Brushes.Gray;
+            Sprite1L.Foreground = Brushes.Gray;
+            Sprite05L.Foreground = Brushes.Gray;
+        }
+
+        private void Fanta05L_Click(object sender, RoutedEventArgs e)
+        {
+            Fanta05L.Foreground= Brushes.DarkOrange;
+            Fanta1L.Foreground = Brushes.Gray;
+            Cola1L.Foreground = Brushes.Gray;
+            Cola05L.Foreground = Brushes.Gray;
+            Sprite1L.Foreground = Brushes.Gray;
+            Sprite05L.Foreground = Brushes.Gray;
+        }
+
+        private void Sprite1L_Click(object sender, RoutedEventArgs e)
+        {
+            Sprite1L.Foreground = Brushes.Green;
+            Sprite05L.Foreground = Brushes.Gray;
+            Cola1L.Foreground = Brushes.Gray;
+            Cola05L.Foreground = Brushes.Gray;
+            Fanta1L.Foreground = Brushes.Gray;
+            Fanta05L.Foreground = Brushes.Gray;
+        }
+
+        private void Sprite05L_Click(object sender, RoutedEventArgs e)
+        {
+            Sprite05L.Foreground = Brushes.Green;
+            Sprite1L.Foreground = Brushes.Gray;
+            Cola1L.Foreground = Brushes.Gray;
+            Cola05L.Foreground = Brushes.Gray;
+            Fanta1L.Foreground = Brushes.Gray;
+            Fanta05L.Foreground = Brushes.Gray;
+        }
+    }
+}
---
+++ WPFStoreApp/Products.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace WPFStoreApp
+{
+    public class Products
+    {
+        public string Name {  get; set; }
+        public double Price {  get; set; }
+        public string Description { get; set; }
+        public Image Image { get; set; }
+
+        public Products(Image image,string name, double price, string description)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+            Image = image;
+        }
+    }
+
+    public class Cola : Products
+    {
+        public Cola(Image image, string name, double price, string description)
+            : base(image ,name, price, description)
+        { }
+    }
+
+    public class Fanta : Products
+    {
+        public Fanta(Image image, string name, double price, string description)
+            : base(image, name, price, description)
+        { }
+    }
+
+    public class Sprite : Products
+    {
+        public Sprite(Image image, string name, double price, string description)
+        : base(image, name, price, description)
+        { }
+    }
+}
